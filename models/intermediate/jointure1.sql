6fe9db01e103ad587458883ab5e92fdb296a4978
Marie DELVART <marie3.lewagon+1@gmail.com>
2024-07-18 09:49:34 +0000

    jointure 1 faite!

--- models/intermediate/jointure1.sql
+++ models/intermediate/jointure1.sql
@@ -4,4 +4,6 @@ JOIN {{ ref("diplome_alice") }}
 USING (numero_contrat)
 
 JOIN {{ ref("gaelle_public_prive") }} USING (numero_contrat)
-#JOIN {{ ref("gaelle_clean_codes_postaux") }} U (alternant_code_postal_clean)
+JOIN {{ ref("gaelle_clean_codes_postaux") }} ON (formation_Arnaud.alternant_code_postal_clean) = (gaelle_clean_codes_postaux.corrected_postal_code)
+
+
