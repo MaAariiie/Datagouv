4aad147fdf145be9d9d28483b65784281c3d845c
Marie DELVART <marie3.lewagon+1@gmail.com>
2024-07-18 14:29:10 +0000

    nmkvd

--- models/intermediate/jointure1.sql
+++ models/intermediate/jointure1.sql
@@ -1,8 +1,8 @@
 SELECT *
 FROM {{ ref("formation_Arnaud") }}
-JOIN {{ ref("diplome_alice") }} USING (numero_contrat)
+INNER JOIN {{ ref("diplome_alice") }} USING (numero_contrat)
 
-JOIN {{ ref("gaelle_public_prive") }} USING (numero_contrat)
-JOIN {{ ref("gaelle_clean_codes_postaux") }} ON (formation_Arnaud.alternant_code_postal_clean) = (gaelle_clean_codes_postaux.corrected_postal_code)
+INNER JOIN {{ ref("gaelle_public_prive") }} USING (numero_contrat)
+INNER JOIN {{ ref("gaelle_clean_codes_postaux") }} ON (formation_Arnaud.alternant_code_postal_clean) = (gaelle_clean_codes_postaux.corrected_postal_code)
 
 
--- models/mart/rupture_jointure_finale.sql
+++ models/mart/rupture_jointure_finale.sql
@@ -3,4 +3,4 @@ SELECT
 jointure1.*,
 jointure_apres_casewhen.categorie_formation
 FROM {{ ref("jointure1") }}
-JOIN {{ ref("jointure_apres_casewhen") }} USING (numero_contrat)
+INNER JOIN {{ ref("jointure_apres_casewhen") }} USING (numero_contrat)
